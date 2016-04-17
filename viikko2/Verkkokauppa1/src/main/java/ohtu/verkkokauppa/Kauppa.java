921af5e907dc97f77b01ca8541fcda1a61c88c76
Antti Leinonen <antti.leinonen@helsinki.fi>
2016-04-17 16:09:11 +0300

    jtn 2 viikon muutoksia jotka unohdin committaa

--- viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -25,13 +25,13 @@ public class Kauppa {
     }
 
     public void poistaKorista(int id) {
-        Tuote t = varasto.haeTuote(id); 
+        Tuote t = varasto.haeTuote(id);
         varasto.palautaVarastoon(t);
     }
 
     public void lisaaKoriin(int id) {
         if (varasto.saldo(id)>0) {
-            Tuote t = varasto.haeTuote(id);             
+            Tuote t = varasto.haeTuote(id);
             ostoskori.lisaa(t);
             varasto.otaVarastosta(t);
         }
@@ -40,12 +40,36 @@ public class Kauppa {
     public boolean tilimaksu(String nimi, String tiliNumero) {
         int viite = viitegeneraattori.uusi();
         int summa = ostoskori.hinta();
-        
+
         return pankki.tilisiirto(nimi, viite, tiliNumero, kaupanTili, summa);
     }
-    
+
     public KirjanpitoRajapinta kirjanpito() {
         return pankki.kirjanpito();
+    }
+
+    public void checkstyletRikkovaMetodi() {
+        if (true)
+        {
+        if (true)
+        {
+        if (true)
+        {
+
+        }
+        }
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+        for (int j = 0; j < 2; j++)
+        {
+        for (int k = 0; k < 3; k++)
+        {
+
+        }
+        }
+        }
     }
 
 }
